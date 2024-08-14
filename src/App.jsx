8e68716f54a871c7d750fb4caf0f9d5342593d03
MolchanovaOlga <designer.molchanova@gmail.com>
2024-08-14 18:03:03 +0300

    Add pages navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Navigation from "./components/Navigation/Navigation";
+import Container from "./components/Container/Container";
 import "./App.css";
 
+const HomePage = lazy(() => import("./pages/Home"));
+const TeachersPage = lazy(() => import("./pages/Teachers"));
+const FavoritesPage = lazy(() => import("./pages/Favorites"));
+const NotFoundPage = lazy(() => import("./pages/NotFound"));
+
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <Navigation />
+      <main>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/teachers" element={<TeachersPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="*" element={<NotFoundPage replace />} />
+          </Routes>
+        </Suspense>
+      </main>
+    </Container>
+  );
 }
 
 export default App;
