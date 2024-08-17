ddedbd78c4f6d02b92a7f3ec0a3b0569cecab6f1
MolchanovaOlga <designer.molchanova@gmail.com>
2024-08-17 20:10:21 +0300

    Change header in mobile

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,17 +3,24 @@ import sprite from "../../assets/sprite.svg";
 
 const AuthNav = () => {
   return (
-    <div className={css.authNavContainer}>
-      <button type="button" className={css.btnLogIn}>
-        <svg className={css.iconLogIn} width="20" height="20">
-          <use href={`${sprite}#icon-log-in`}></use>
+    <>
+      <div className={css.authNavContainer}>
+        <button type="button" className={css.btnLogIn}>
+          <svg className={css.iconLogIn} width="20" height="20">
+            <use href={`${sprite}#icon-log-in`}></use>
+          </svg>
+          Log In
+        </button>
+        <button type="button" className={css.btnRegister}>
+          Registration
+        </button>
+      </div>
+      <button type="button" className={css.btnAuthNav}>
+        <svg className={css.iconThreeDots} width="20" height="20">
+          <use href={`${sprite}#icon-dots-three-vertical`}></use>
         </svg>
-        Log In
       </button>
-      <button type="button" className={css.btnRegister}>
-        Registration
-      </button>
-    </div>
+    </>
   );
 };
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { NavLink } from "react-router-dom";
 
+import sprite from "../../assets/sprite.svg";
 import css from "./Navigation.module.css";
 
 const Navigation = () => {
@@ -8,17 +9,24 @@ const Navigation = () => {
     return clsx(css.link, isActive && css.active);
   };
   return (
-    <nav className={css.nav}>
-      <NavLink to="/" className={buildLinkClass}>
-        Home
-      </NavLink>
-      <NavLink to="/teachers" className={buildLinkClass}>
-        Teachers
-      </NavLink>
-      <NavLink to="/favorites" className={buildLinkClass}>
-        Favorites
-      </NavLink>
-    </nav>
+    <>
+      <nav className={css.nav}>
+        <NavLink to="/" className={buildLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/teachers" className={buildLinkClass}>
+          Teachers
+        </NavLink>
+        <NavLink to="/favorites" className={buildLinkClass}>
+          Favorites
+        </NavLink>
+      </nav>
+      <button type="button" className={css.btnMenu}>
+        <svg className={css.iconMenu} width="20" height="20">
+          <use href={`${sprite}#icon-menu`}></use>
+        </svg>
+      </button>
+    </>
   );
 };
 
