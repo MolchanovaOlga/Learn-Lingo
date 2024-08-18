69667273c898585022246820f502253a2b2debda
MolchanovaOlga <designer.molchanova@gmail.com>
2024-08-18 12:12:54 +0300

    Add mobile menus for authentification and navigation

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+
+import scrollController from "../../services/noScroll";
+import { closeByEsc } from "../../services/functions";
 import css from "./AuthNav.module.css";
 import sprite from "../../assets/sprite.svg";
 
 const AuthNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openMenu() {
+    setIsOpen(true);
+    scrollController.disabledScroll();
+  }
+
+  function closeMenu() {
+    setIsOpen(false);
+    scrollController.enabledScroll();
+  }
+
+  useEffect(() => {
+    closeByEsc(closeMenu);
+  }, []);
   return (
     <>
       <div className={css.authNavContainer}>
@@ -15,11 +34,29 @@ const AuthNav = () => {
           Registration
         </button>
       </div>
-      <button type="button" className={css.btnAuthNav}>
-        <svg className={css.iconThreeDots} width="20" height="20">
-          <use href={`${sprite}#icon-dots-three-vertical`}></use>
-        </svg>
-      </button>
+      <div className={css.containerMenu}>
+        <button type="button" className={css.btnAuthNav} onClick={openMenu}>
+          <svg className={css.iconThreeDots} width="20" height="20">
+            <use href={`${sprite}#icon-dots-three-vertical`}></use>
+          </svg>
+        </button>
+
+        {isOpen && <div className={css.backdrop} onClick={closeMenu}></div>}
+
+        {isOpen && (
+          <div className={css.btnsContainer}>
+            <button type="button" className={css.btnLogIn}>
+              <svg className={css.iconLogIn} width="20" height="20">
+                <use href={`${sprite}#icon-log-in`}></use>
+              </svg>
+              Log In
+            </button>
+            <button type="button" className={css.btnRegister}>
+              Registration
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+import scrollController from "../../services/noScroll";
+import { closeByEsc } from "../../services/functions";
 import sprite from "../../assets/sprite.svg";
 import css from "./Navigation.module.css";
 
@@ -8,6 +11,23 @@ const Navigation = () => {
   const buildLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
   };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openMenu() {
+    setIsOpen(true);
+    scrollController.disabledScroll();
+  }
+
+  function closeMenu() {
+    setIsOpen(false);
+    scrollController.enabledScroll();
+  }
+
+  useEffect(() => {
+    closeByEsc(closeMenu);
+  }, []);
+
   return (
     <>
       <nav className={css.nav}>
@@ -21,11 +41,43 @@ const Navigation = () => {
           Favorites
         </NavLink>
       </nav>
-      <button type="button" className={css.btnMenu}>
-        <svg className={css.iconMenu} width="20" height="20">
-          <use href={`${sprite}#icon-menu`}></use>
-        </svg>
-      </button>
+      <div className={css.containerMenu}>
+        <button onClick={openMenu} type="button" className={css.btnMenu}>
+          <svg className={css.iconMenu} width="20" height="20">
+            <use href={`${sprite}#icon-menu`}></use>
+          </svg>
+        </button>
+
+        {isOpen && <div className={css.backdrop} onClick={closeMenu}></div>}
+
+        {isOpen && (
+          <nav className={css.navMenu}>
+            <div className={css.menuItemContainer}>
+              <NavLink to="/" className={buildLinkClass} onClick={closeMenu}>
+                Home
+              </NavLink>
+            </div>
+            <div className={css.menuItemContainer}>
+              <NavLink
+                to="/teachers"
+                className={buildLinkClass}
+                onClick={closeMenu}
+              >
+                Teachers
+              </NavLink>
+            </div>
+            <div className={css.menuItemContainer}>
+              <NavLink
+                to="/favorites"
+                className={buildLinkClass}
+                onClick={closeMenu}
+              >
+                Favorites
+              </NavLink>
+            </div>
+          </nav>
+        )}
+      </div>
     </>
   );
 };
