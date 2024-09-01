a7a4ff3bce18f07176031f5dbde5e415ecd4ffe4
MolchanovaOlga <designer.molchanova@gmail.com>
2024-09-02 02:18:59 +0300

    Change firebaseConfig

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
+  apiKey: "AIzaSyDLYZmRLr0imJmnsHgCpdjJpMi6-gqPi90",
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_DATABASE_URL,
   projectId: import.meta.env.VITE_PROJECT_ID,
